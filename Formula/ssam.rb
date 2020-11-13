50165a8722ea0ea6b23bd4e801fdb23dab3e6ecf
Ryad ZENINE <r.zenine@gmail.com>
2020-11-13 17:14:45 +0100

    Revert "bump version"
    This reverts commit 6c71fbdb5f9389241a1c155bb98e641a4f5be95b.

--- Formula/ssam.rb
+++ Formula/ssam.rb
@@ -5,7 +5,7 @@ class Ssam < Formula
     desc "sam lets you difine custom aliases and search them using fuzzy search."
     homepage "https://github.com/r-zenine/ssam"
     url "https://github.com/r-zenine/ssam/releases/download/v0.7.11/sam_macos_x86_64_v0.7.11.tar.gz"
-    sha256 ""
+    sha256 "cd710a9102c1e41c1a647087160be9e1ad51ae3123db1718aa29a934024c5324"
     version "0.7.11"
   
     def install
