ed673290e513f57f6cc97d8cfcd7dbd297cb0cd8
Ryad ZENINE <r.zenine@gmail.com>
2020-12-13 18:10:58 +0000

    bump version

--- Formula/sam.rb
+++ Formula/sam.rb
@@ -4,9 +4,9 @@
 class Sam < Formula
     desc "sam lets you difine custom aliases and search them using fuzzy search."
     homepage "https://github.com/r-zenine/sam"
-    url "https://github.com/r-zenine/sam/releases/download/v0.7.15/sam_macos_x86_64_v0.7.15.tar.gz"
-    sha256 "3638061d8ebab8f155ebb49f7331b2f8aee152812068aee0147dc54f864cc440"
-    version "0.7.15"
+    url "https://github.com/r-zenine/sam/releases/download/v0.7.17/sam_macos_x86_64_v0.7.17.tar.gz"
+    sha256 "b9bd840f722c16a124dbee21d57965f72dfc05b8ee3e39040c6298e978191e43"
+    version "0.7.17"
   
     def install
       bin.install "sam"
